ba55c89b26501cf8af3fe417f6df49f83064d33c
Kareem Ragab <kragab538@gmail.com>
2025-08-23 17:29:11 +0300

    add the routing for write content icon and profile picture

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,17 @@
-<<<<<<< HEAD
-import HeroSection from "./Hero"
-=======
+
 import CallToAction from "../../components/Home/CallToAction";
 import FeaturedInsights from "../../components/Home/FeaturedInsights";
 import HeroSection from "../../components/Home/HeroSection";
 import HowItWorks from "../../components/Home/HowItWorks";
 import TrendingCategories from "../../components/Home/TrendingCategories";
->>>>>>> 9003d93244f7322672cbf14dfd7bc9b533c8c0eb
 
 const Home = () => {
   return (
     <>
-<<<<<<< HEAD
-      <HeroSection />
-    </>
-  )
-}
-=======
+     
       {/* Hero */}
       <HeroSection />
->>>>>>> 9003d93244f7322672cbf14dfd7bc9b533c8c0eb
+ 
 
       {/* Featured Insights*/}
       <FeaturedInsights />
